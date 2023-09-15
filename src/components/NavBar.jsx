ae1a2e09e78eb8e2445862397c5d7e272c82dadb
Oriyan Nadav <oriyanadav5@gmail.com>
2023-09-15 11:51:15 +1000

    navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import { Link, Outlet } from "react-router-dom";
 import useAuth from "../hooks/use-auth.js";
+import "./NavBar.css";
 
 function NavBar() {
     const {auth, setAuth} = useAuth();
@@ -9,21 +10,25 @@ function NavBar() {
         setAuth({ token: null });
     };
 
-    console.log(auth)
-
     return (
         <div>
             <div>
-                <nav>
-                    <Link to="/">Home</Link>
-                    {auth.token ? (
-                        <Link to="/" onClick={handleLogout}>
-                            Log Out
-                        </Link>
-                    ) : (
-                        <Link to="/login">Login</Link>
-                    )}
-                </nav>
+                <header className="header">
+                    <Link to="/" className="logo"><img src="../../public/logo2.png" alt="" /></Link>
+                    <div className="menu-btn">
+                        <div className="menu-burger"></div>
+                    </div>
+                    <nav className="navbar flex-nav" id="navbar">
+                        <Link to="/">Home</Link>
+                        {auth.token ? (
+                            <Link to="/" onClick={handleLogout}>
+                                Log Out
+                            </Link>
+                        ) : (
+                            <Link to="/login">Login</Link>
+                        )}
+                    </nav>
+                </header>
                 <Outlet />
             </div>
         </div>  
