558aa33192cf40a4d9f2caef7a6cc0109c877852
Oriyan Nadav <oriyanadav5@gmail.com>
2023-09-21 22:14:01 +1000

    home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,54 +1,28 @@
-import { useEffect, useRef } from 'react';
-import { register } from 'swiper/element/bundle';
-import Swiper from 'swiper';
+import { useRef } from 'react'
+
+import { Autoplay, EffectCoverflow, Navigation, Pagination } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/parallax';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 import useProjects from "../hooks/use-projects";
 import ProjectCard from "../components/ProjectCard";
 import "./HomePage.css";
 
+
 function HomePage() {
     const { projects, isLoading, error } = useProjects();
-    const swiperRef = useRef(null);
 
-    useEffect(() => {
-        // Register Swiper web component
-        register();
-
-        // Add event listener
-        if (swiperRef.current && document.querySelector('.tranding-slider')) {
-            const trandingSlider = new Swiper('.tranding-slider', {
-                effect: 'coverflow',
-                grabCursor: true,
-                centeredSlides: true,
-                loop: true,
-                slidesPerView: 'auto',
-                coverflowEffect: {
-                    rotate: 0,
-                    stretch: 0,
-                    depth: 100,
-                    modifier: 2.5,
-                },
-                pagination: {
-                    el: '.swiper-pagination',
-                    clickable: true,
-                },
-                navigation: {
-                    nextEl: '.swiper-button-next',
-                    prevEl: '.swiper-button-prev',
-                }
-            });
-
-            // Add event listener
-            trandingSlider.on('slideChange', () => {
-                console.log('Slide changed');
-            });
-
-            // Cleanup Swiper when the component unmounts
-            return () => {
-                trandingSlider.destroy(true, true);
-            };
-        }
-    }, []);
+    const progressCircle = useRef(null);
+    const progressContent = useRef(null);
+    const onAutoplayTimeLeft = (s, time, progress) => {
+        progressCircle.current.style.setProperty('--progress', 1 - progress);
+        progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+    };
 
     if (isLoading) {
         return (<p>loading...</p>)
@@ -57,22 +31,64 @@ function HomePage() {
     if (error) {
         return (<p>Error is: {error.message}</p>)
     }
-    
+
     return (
         <section className="section home">
             <div className="welcome">
                 <img className="header-image" src="../../graduation3.jpeg" alt="" />
                 <div className="overlay-home"></div>
-                {/* <img className="schoolr-image" src="../../schoolr.png" alt="" /> */}
             </div>
-            {/* <swiper-container init="false" ref={swiperRef}> */}
-                {/* <div id="project-list"> */}
+            <div className="swiper-projects">
+                <Swiper
+                    modules = {[EffectCoverflow, Autoplay, Pagination, Navigation]}
+                    coverflowEffect={
+                        {
+                            depth:500,
+                            rotate: 50,
+                            stretch: 0,
+                            modifier: 1,
+                            slideShadows: true,
+                        }
+                    }
+                    autoplay={{
+                        delay: 3500,
+                        disableOnInteraction: false,
+                    }}
+                    pagination={
+                        {
+                            clickable: true,
+                            dynamicBullets: true,
+                        }
+                    }
+                    centeredSlides={true}
+                    effect='coverflow'
+                    grabCursor={true}
+                    // navigation={true}
+                    slidesPerView={3}
+                    className="mySwiper"
+                    spaceBetween={20}
+                    onAutoplayTimeLeft={onAutoplayTimeLeft}
+                    onSlideChange={() => console.log('slide change')}
+                    onSwiper={(swiper) => console.log(swiper)}
+                >
+                    
                     {
                         projects.map((project, key) => {
-                            return <ProjectCard key={key} projectData={project} />;
+                            return (
+                                <SwiperSlide key={key}>
+                                    <ProjectCard key={key} projectData={project} />
+                                </SwiperSlide>
+                            )
                     })}
-                {/* </div> */}
-            {/* </swiper-container> */}
+                    
+                    <div className="autoplay-progress" slot="container-end">
+                        <svg viewBox="0 0 48 48" ref={progressCircle}>
+                            <circle cx="24" cy="24" r="20"></circle>
+                        </svg>
+                        <span ref={progressContent}></span>
+                    </div>
+                </Swiper>
+            </div>
         </section>
     );
 }
