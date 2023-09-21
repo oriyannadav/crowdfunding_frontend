d351cb2caf55f316c26bfe9613c517b8047b0d63
Oriyan Nadav <oriyanadav5@gmail.com>
2023-09-21 22:13:48 +1000

    navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,28 +7,39 @@ function NavBar() {
 
     const handleLogout = () => {
         window.localStorage.removeItem("token");
-        setAuth({ token: null });
+        setAuth({
+            token: null,
+            userId: null
+        });
     };
+
+    const toggleMenu = (event) => {
+        event.target.classList.add('open')
+    }
 
     return (
         <div>
             <div>
                 <header className="header">
                     <Link to="/" className="logo"><img src="../../schoolr1.png" alt="" /></Link>
-                    <div className="menu-btn">
+                    <div className="menu-btn" onClick={toggleMenu}>
                         <div className="menu-burger"></div>
                     </div>
                     <nav className="navbar flex-nav" id="navbar">
                         <Link to="/">Home</Link>
                         {auth.token ? (
+                            <>
                             <Link to="/" onClick={handleLogout}>
                                 Log Out
                             </Link>
+                            <Link to="/profile">Profile</Link>
+                            </>
                         ) : (
-                            
-                            <Link to="/login">Login</Link>
+                            <>
+                            <Link to="/login">Log In</Link>
+                            <Link to="/signup">Sign Up</Link>
+                            </>
                         )}
-                        <Link to="/signup">Sign Up</Link>
                     </nav>
                 </header>
                 <Outlet />
